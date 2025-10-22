5580fa9c27a44225f635d1b05ff027b7b28c76b2
Macwell <blood_hiphop@hotmail.it>
2025-10-23 01:15:18 +0200

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'beautiful-recreation-production.up.railway.app/api' // Your actual production API URL
+  apiUrl: 'https://beautiful-recreation-production.up.railway.app/api' // Your actual production API URL
 };
